181dbbce2aef8a5b0e7b04263ed3b37bbb51e6b6
Dieta Hanson <dietahanson@gmail.com>
2018-05-24 12:49:19 -0700

    Revert "Revert "groups""
    This reverts commit daf4cfd707292f79e0ab118ef09288e25c0cd63a.

--- R_code/network_work.R
+++ R_code/network_work.R
@@ -1,8 +1,48 @@
 ###############################################
-###Put Stefani's groups into her network
+###Add groups to network
 ###############################################
 
-library(reshape2)
+
+stefgroup <- read.csv("stefgroups.csv",  #get stefanis groups 
+                      header = T,
+                      stringsAsFactors = F) 
+
+tg <- read.delim("tg_martu.txt", #get trophic groups from n_w
+                 header = T, 
+                 stringsAsFactors = F,
+                 sep = "\t")
+
+tg$Species <- gsub("_", " ", tg$Species)  #fix species names
+tg$Species <- gsub("human", "Homo sapiens", tg$Species)
+tg$Species <- gsub("\\*", "", tg$Species)
+
+aic <- read.delim("aic_martu.txt", #get aic groups from n_w
+                 header = T, 
+                 stringsAsFactors = F,
+                 sep = "\t")
+
+aic$Species <- gsub("_", " ", aic$Species) #fix species names
+aic$Species <- gsub("human", "Homo sapiens", aic$Species)
+aic$Species <- gsub("\\*", "", aic$Species)
+
+groups <- data.frame(common = stefgroup$common, #make table of species names
+                     latin = stefgroup$latin,
+                     stefgroup = stefgroup$stefgroup)
+
+groups$tg <- tg$X.TroG[match(groups$latin, tg$Species)] #add trophic groups
+
+groups$aic <- aic$X.AicG[match(groups$latin, aic$Species)] #add aic groups
+
+groups[is.na(groups$tg),] #returns species with no group match in tg
+groups[is.na(groups$aic),] #returns species with no group match in aic
+
+
+
+
+###################################################################
+#####Make input for Loop Analysis
+###################################################################
+
 
 
 hotnet <- read.csv("hotnet.csv",  #get network
@@ -11,38 +51,30 @@ hotnet <- read.csv("hotnet.csv",  #get network
 
 hotnet$weight <- NULL #take away weights
 
-stefgroup <- read.csv("stefgroups.csv",  #get groups 
-                      header = T,
-                      stringsAsFactors = F) 
-
-stefgroup<-melt(stefgroup, #melt common names and latin names
-                id.vars = "Group",
-                value.name = "name")
-                                                                          
 
 
-hotnet$From <- as.factor(stefgroup$Group[match(hotnet$predator, #match predators
-                                               stefgroup$name)])
+##Change which groups you want to use (stefani's, tg, or aic) by changing the
+##groups column that is selected
+
+hotnet$From <- as.factor(groups$tg[match(hotnet$predator, #match predators
+                                               groups$latin)])
 
 cat(sum(is.na(hotnet$From)), #print how many did not match
-    "predators with missing matches:",
-    hotnet[is.na(hotnet$From)])
+    "predators with missing matches:")
+hotnet[is.na(hotnet$From),]
 
-hotnet$To <- as.factor(stefgroup$Group[match(hotnet$prey,   #match prey
-                                           stefgroup$name)])
+hotnet$To <- as.factor(groups$tg[match(hotnet$prey,   #match prey
+                                           groups$latin)])
 
 cat(sum(is.na(hotnet$To)), #print how many did not match
-    "prey with missing matches:",
-    hotnet[is.na(hotnet$To)])
+    "prey with missing matches:")
+    hotnet[is.na(hotnet$To),]
 
 hotnet$Type <- "Predator-prey" #add interaction type
 
 #write.csv(hotnet, file = "hotgrouped.csv", row.names = F)
 
 
-################################################
-###Make a 
-################################################
 
 
 
